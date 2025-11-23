3d82ea1e70dadc55f485e1ccc8c54262126f5095
eunjiiiiiiiiii <eeungg02@gmail.com>
2025-11-23 16:43:38 +0900

    no message

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,7 +62,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:8080", "http://127.0.0.1:8080"],
+        allow_origins=["http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:8081", "http://127.0.0.1:8081"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
